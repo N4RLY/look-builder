920f2926cfa8d21c87f8a5c91a0124bac401b71a
N4RLY <aleminee2@gmail.com>
2025-05-10 14:19:55 +0300

    add mock loading

--- screens/loading.py
+++ screens/loading.py
@@ -1,22 +1,24 @@
 import streamlit as st
 from utils.session_state import SessionState
+import time
 
 def render():
-    """Render the Loading screen"""
-    st.header("Screen 3: Loading")
+    """Render the Loading screen with a mock delay, then go to suggested_items."""
+    st.header("")
     
     # Center the loading message and spinner
     col1, col2, col3 = st.columns([1, 2, 1])
     
     with col2:
         st.write("")
-        st.write("")
-        st.markdown("<h3 style='text-align: center;'>Finding the perfect look for you...</h3>", unsafe_allow_html=True)
-        st.write("")
-        
+
         # Display a spinner
-        with st.spinner():
-            st.write("")  # This is just to give some space for the spinner
+        with st.spinner("Finding the perfect look for you..."):
+            time.sleep(2)
+        
+        # After loading, go to suggested_items
+        SessionState.navigate_to("suggested_items")
+        st.rerun()
         
         # The actual navigation happens in the suggested_items.py when the button is clicked
         # This is just a visual screen that will be shown briefly 
